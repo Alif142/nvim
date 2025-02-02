740c43cdf5b236a3b3dcd96431b0b6c066b7cf23
Alif142 <mizan.alif190@gmail.com>
2025-02-02 10:04:03 +0600

    Changed Harpoon binding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,9 +12,9 @@ return{
       vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Harpoon: Toggle menu", silent = true })
 
       -- Navigating to specific Harpoon slots
-      vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end, { desc = "Harpoon: File 1", silent = true })
-      vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end, { desc = "Harpoon: File 2", silent = true })
-      vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end, { desc = "Harpoon: File 3", silent = true })
-      vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end, { desc = "Harpoon: File 4", silent = true })
+      vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end, { desc = "Harpoon: File 1", silent = true })
+      vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end, { desc = "Harpoon: File 2", silent = true })
+      vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end, { desc = "Harpoon: File 3", silent = true })
+      vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end, { desc = "Harpoon: File 4", silent = true })
     end
 }
