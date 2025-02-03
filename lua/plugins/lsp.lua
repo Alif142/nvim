5e5bdceb5607e6281ada2e261b6b77dee5535856
Alif142 <mizan.alif190@gmail.com>
2025-02-03 16:46:31 +0600

    Added files for completions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,38 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
+    {
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+        config = function()
+            require("mason").setup()
+        end,
     },
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+    {
+        "williamboman/mason-lspconfig.nvim",
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls","jdtls","clangd"
-            },
-            handlers = {
-                function(lua_ls) -- default handler (optional)
-                    require("lspconfig")[lua_ls].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-
-                function(clangd) -- default handler (optional)
-                    require("lspconfig")[clangd].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-
-                function(jdtls) -- default handler (optional)
-                    require("lspconfig")[jdtls].setup {
-                        capabilities = capabilities
-                    }
-                end,
-            }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd", "jdtls" },
             })
-        })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({capabilities = capabilities})
+            lspconfig.jdtls.setup({capabilities = capabilities})
+            lspconfig.clangd.setup({capabilities = capabilities})
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
+            vim.keymap.set("n", "<leader>H", function()
+                vim.lsp.buf.hover()
+            end)
+            vim.keymap.set("n", "<leader>gd", function()
+                vim.lsp.buf.defination()
+            end)
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.lsp.buf.code_action()
+            end)
+        end,
+    },
 }
-
-
