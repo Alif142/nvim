5e28b631ed56ddc9e3a4e1b09cbef23bc23e37f6
Alif142 <mizan.alif190@gmail.com>
2025-02-06 23:43:53 +0600

    Auto comp change

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,9 +20,9 @@ return {
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({capabilities = capabilities})
-            lspconfig.jdtls.setup({capabilities = capabilities})
-            lspconfig.clangd.setup({capabilities = capabilities})
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.jdtls.setup({ capabilities = capabilities })
+            lspconfig.clangd.setup({ capabilities = capabilities })
 
             vim.keymap.set("n", "<leader>H", function()
                 vim.lsp.buf.hover()
