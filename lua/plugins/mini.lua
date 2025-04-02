d7e25ab2014f9736ae09e8644c50424f1d683d45
Alif142 <mizan.alif190@gmail.com>
2025-04-02 15:20:46 +0600

    Updated mini nvim and snacks nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'echasnovski/mini.nvim',
+        version = false,
+        config = function()
+            require('mini.ai').setup()
+            require('mini.tabline').setup()
+            require('mini.statusline').setup()
+            require('mini.pairs').setup()
+        end,
+    }
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,13 +11,6 @@ return {
                 },
             },
         })
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
-
-        vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope help tags" })
-        vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "Telescope help tags" })
     end,
 }
+
