a6492288de930ad9f748fd9404d9b1dcf9c31a4f
Alif142 <mizan.alif190@gmail.com>
2025-06-01 13:10:27 +0600

    updared harpoon keybinds

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,12 +12,12 @@ return{
       vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Harpoon: Toggle menu", silent = true })
 
       -- Navigating to specific Harpoon slots
-      vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end, { desc = "Harpoon: File 1", silent = true })
-      vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end, { desc = "Harpoon: File 2", silent = true })
-      vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end, { desc = "Harpoon: File 3", silent = true })
-      vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end, { desc = "Harpoon: File 4", silent = true })
-      vim.keymap.set("n", "<M-5>", function() ui.nav_file(5) end, { desc = "Harpoon: File 5", silent = true })
-      vim.keymap.set("n", "<M-6>", function() ui.nav_file(6) end, { desc = "Harpoon: File 6", silent = true })
-      vim.keymap.set("n", "<M-7>", function() ui.nav_file(7) end, { desc = "Harpoon: File 7", silent = true })
+      vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon: File 1", silent = true })
+      vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon: File 2", silent = true })
+      vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon: File 3", silent = true })
+      vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon: File 4", silent = true })
+      vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end, { desc = "Harpoon: File 5", silent = true })
+      vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end, { desc = "Harpoon: File 6", silent = true })
+      vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end, { desc = "Harpoon: File 7", silent = true })
     end
 }
